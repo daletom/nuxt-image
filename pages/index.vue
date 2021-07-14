a3606f54ab37011cc43aa1b455d2190c4ab55e4a
daletom <tom@imgix.com>
2021-07-13 23:28:02 -0700

    update urls and client hints

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="title-font sm:text-4xl text-3xl m-4 font-medium text-gray-900">Example using the Nuxt Image Component</h1>
-    <p class="m-4 leading-relaxed">Optimize your images in your <a href="https://image.nuxtjs.org/providers/imgix" class="mt-3 text-indigo-500 inline-flex">Nuxt Image Component</a> using imgix. For more info about how this tutorial was made, you can view the accompanying <a href="https://image.nuxtjs.org/providers/imgix" class="mt-3 text-indigo-500 inline-flex">blog post</a> or you can view the code directly on Github.  You can also find out more about the imgix parameters in the imgix <a href="https://docs.imgix.com/apis/rendering" class="mt-3 text-indigo-500 inline-flex">documenation.</a> </p>
+    <p class="m-4 leading-relaxed">Optimize your images in your <a href="https://image.nuxtjs.org/providers/imgix" class="mt-3 text-indigo-500 inline-flex">Nuxt Image Component</a> using imgix. For more info about how this tutorial was made, you can view the accompanying <a href="https://image.nuxtjs.org/providers/imgix" class="mt-3 text-indigo-500 inline-flex">blog post</a> or you can view the code directly on <a href="https://github.com/daletom/nuxt-image" class="mt-3 text-indigo-500 inline-flex">Github.</a>  You can also find out more about the imgix parameters in the imgix <a href="https://docs.imgix.com/apis/rendering" class="mt-3 text-indigo-500 inline-flex">documenation.</a> </p>
     <div class="flex flex-wrap">
       <nuxt-img
         class="float-left p-2 m-auto w-full lg:w-1/2 xl:w-1/3 2xl:w-1/3"
@@ -11,7 +11,7 @@
         :src="image"
         sizes="xs:98vw sm:98vw md:98vw lg:49vw xl:31vw 2xl:31vw"
         fit="crop"
-        :modifiers="{ auto: 'format,compress', ar: '1:1' }"
+        :modifiers="{ auto: 'format,compress', ar: '1:1', ch: 'width,DPR' }"
       />
     </div>
   </div>
